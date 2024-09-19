d7a9a085278b619ccd19812bd1312f72648a52c8
Your Name <anesios98@gmail.com>
2024-09-19 20:35:28 -0300

    refatoração do arquivo

--- accelteste_01.c
+++ accelteste_01.c
@@ -1,12 +1,10 @@
 #	ifndef accel_H
 #define accel_H
-//#include "ADXL345.h"
 #include <inttypes.h>
 #include <stdio.h>
 #include <stdint.h>
 #include <string.h>
 #include "ADXL345.h"
-
 
 // ADXL345 Functions
 void ADXL345_Init();
@@ -18,24 +16,55 @@ void ADXL345_IdRead(uint8_t *pId);
 void ADXL345_REG_READ(uint8_t address, uint8_t *value);
 void ADXL345_REG_WRITE(uint8_t address, uint8_t value);
 void ADXL345_REG_MULTI_READ(uint8_t address, uint8_t values[], uint8_t len);
-
 // I2C0 Functions
 void I2C0_Init();
 void I2C0_Enable_FPGA_Access();
-
 // Pinmux Functions
 void Pinmux_Config();
 
+
+
+int main(void){
+    uint8_t devid;
+    int16_t mg_per_lsb = 4;
+    int16_t XYZ[3];
+
+    // Configure Pin Muxing
+    Pinmux_Config();
+
+    // Initialize I2C0 Controller
+    I2C0_Init();
+
+    // 0xE5 is read from DEVID(0x00) if I2C is functioning correctly
+    ADXL345_REG_READ(0x00, &devid);
+
+    // Correct Device ID
+    if (devid == 0xE5){
+        // Initialize accelerometer chip
+        ADXL345_Init();
+
+        while(1){
+            if (ADXL345_WasActivityUpdated()){
+                ADXL345_XYZ_Read(XYZ);
+                printf("X=%d mg, Y=%d mg, Z=%d mg\n", XYZ[0]*mg_per_lsb, XYZ[1]*mg_per_lsb, XYZ[2]*mg_per_lsb);
+            }
+        }
+    } else {
+        printf("Incorrect device ID\n");
+    }
+
+    return 0;
+}
 void Pinmux_Config(){
         *SYSMGR_I2C0USEFPGA = 0;
         *SYSMGR_GENERALIO7 = 1;
         *SYSMGR_GENERALIO8 = 1;
- }
+}
 
 void I2C0_Init(){
 
- // Abort any ongoing transmits and disable I2C0.
- *I2C0_ENABLE = 2;
+    // Abort any ongoing transmits and disable I2C0.
+    *I2C0_ENABLE = 2;
 
  // Wait until I2C0 is disabled
  while(((*I2C0_ENABLE_STATUS)&0x1) == 1){}
@@ -86,29 +115,29 @@ void ADXL345_REG_WRITE(uint8_t address, uint8_t value){
 }
 
 // Initialize the ADXL345 chip
- void ADXL345_Init(){
+void ADXL345_Init(){
 
- // +- 16g range, full resolution
- ADXL345_REG_WRITE(ADXL345_REG_DATA_FORMAT, XL345_RANGE_16G |
-XL345_FULL_RESOLUTION);
+    // +- 16g range, full resolution
+    ADXL345_REG_WRITE(ADXL345_REG_DATA_FORMAT, XL345_RANGE_16G |
+            XL345_FULL_RESOLUTION);
 
- // Output Data Rate: 200Hz
- ADXL345_REG_WRITE(ADXL345_REG_BW_RATE, XL345_RATE_200);
+    // Output Data Rate: 200Hz
+    ADXL345_REG_WRITE(ADXL345_REG_BW_RATE, XL345_RATE_200);
 
- // The DATA_READY bit is not reliable. It is updated at a much higher rate than the Data Rate
- // Use the Activity and Inactivity interrupts as indicators for new data.
- ADXL345_REG_WRITE(ADXL345_REG_THRESH_ACT, 0x04); //activity threshold
- ADXL345_REG_WRITE(ADXL345_REG_THRESH_INACT, 0x02); //inactivity threshold
- ADXL345_REG_WRITE(ADXL345_REG_TIME_INACT, 0x02); //time for inactivity
- ADXL345_REG_WRITE(ADXL345_REG_ACT_INACT_CTL, 0xFF); //Enables AC coupling for thresholds
- ADXL345_REG_WRITE(ADXL345_REG_INT_ENABLE, XL345_ACTIVITY | XL345_INACTIVITY ); //enable interrupts
+    // The DATA_READY bit is not reliable. It is updated at a much higher rate than the Data Rate
+    // Use the Activity and Inactivity interrupts as indicators for new data.
+    ADXL345_REG_WRITE(ADXL345_REG_THRESH_ACT, 0x04); //activity threshold
+    ADXL345_REG_WRITE(ADXL345_REG_THRESH_INACT, 0x02); //inactivity threshold
+    ADXL345_REG_WRITE(ADXL345_REG_TIME_INACT, 0x02); //time for inactivity
+    ADXL345_REG_WRITE(ADXL345_REG_ACT_INACT_CTL, 0xFF); //Enables AC coupling for thresholds
+    ADXL345_REG_WRITE(ADXL345_REG_INT_ENABLE, XL345_ACTIVITY | XL345_INACTIVITY ); //enable interrupts
 
- // stop measure
-ADXL345_REG_WRITE(ADXL345_REG_POWER_CTL, XL345_STANDBY);
+    // stop measure
+    ADXL345_REG_WRITE(ADXL345_REG_POWER_CTL, XL345_STANDBY);
 
- // start measure
- ADXL345_REG_WRITE(ADXL345_REG_POWER_CTL, XL345_MEASURE);
- }
+    // start measure
+    ADXL345_REG_WRITE(ADXL345_REG_POWER_CTL, XL345_MEASURE);
+}
 
 
 // Return true if there was activity since the last read (checks ACTIVITY bit).
@@ -153,38 +182,6 @@ void ADXL345_REG_MULTI_READ(uint8_t address, uint8_t values[], uint8_t len){
             len--;
         }
     }
-}
-
-int main(void){
-    uint8_t devid;
-    int16_t mg_per_lsb = 4;
-    int16_t XYZ[3];
-
-    // Configure Pin Muxing
-    Pinmux_Config();
-
-    // Initialize I2C0 Controller
-    I2C0_Init();
-
-    // 0xE5 is read from DEVID(0x00) if I2C is functioning correctly
-    ADXL345_REG_READ(0x00, &devid);
-
-    // Correct Device ID
-    if (devid == 0xE5){
-        // Initialize accelerometer chip
-        ADXL345_Init();
-
-        while(1){
-            if (ADXL345_WasActivityUpdated()){
-                ADXL345_XYZ_Read(XYZ);
-                printf("X=%d mg, Y=%d mg, Z=%d mg\n", XYZ[0]*mg_per_lsb, XYZ[1]*mg_per_lsb, XYZ[2]*mg_per_lsb);
-            }
-        }
-    } else {
-        printf("Incorrect device ID\n");
-    }
-
-    return 0;
 }
 
 #endif
